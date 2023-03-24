a3d82bfdfc72c3be19e2e2807eb30575f4a522ac
TranThangBin <thangdev04@gmail.com>
2023-03-24 21:54:06 +0700

    fixed menu height on mobile & rm unecessity

--- src/script.js
+++ src/script.js
@@ -1,31 +1,3 @@
-const navBar = document.getElementById("nav");
-
-const homeElement = document.getElementById("home");
-const aboutElement = document.getElementById("about");
-const contactElement = document.getElementById("contact");
-
-setHeightAll();
-
-window.addEventListener("resize", setHeightAll);
-
-function setHeight(element) {
-    const minHeight = 420;
-    const elementHeight = element.height;
-    const navHeight = navBar.offsetHeight;
-    const viewHeight = window.innerHeight;
-    const expectedHeight = viewHeight - navHeight;
-    if (expectedHeight < minHeight) element.style.height = `${minHeight}px`;
-    else element.style.height = `${expectedHeight}px`;
-}
-
-function setHeightAll() {
-    setHeight(homeElement);
-    setHeight(aboutElement);
-    setHeight(contactElement);
-}
-
-//-------------------------------------------------------------------------------
-
 setCollegeYear();
 setCopyrightYear();
 
